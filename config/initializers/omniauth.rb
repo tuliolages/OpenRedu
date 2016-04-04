593b0a441eb8a896c8241f7fc9a566feb272d4b3
Túlio Paulo Lages da Siva <tulio.plds@gmail.com>
2016-04-03 21:27:06 -0300

    Atualização do Gemfile.lock e do omniauth.rb para usar a versão 3.0.0 do facebook-omniauth e consertar o login via fb.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 Rails.application.config.middleware.use OmniAuth::Builder do
   Redu::Application.config.omniauth.each do |service, app_info|
-    provider service, app_info[:app_id], app_info[:app_secret] 
+    provider service, app_info[:app_id], app_info[:app_secret], :scope => 'email', :display => 'popup'
   end
 end
